22edccf2633346f97166bf5977fa1d69439b4d8f
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-06 17:40:04 -0500

    Introduce generics and remove the APIView

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CourseListCreate.as_view(), name='course_list')
+    path('', views.ListCreateCourse.as_view(), name='course_list')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,19 +1,9 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 
 from . import models
 from . import serializers
 
 
-class CourseListCreate(APIView):
-    def get(self, request, format=None):
-        courses = models.Course.objects.all()
-        serializer = serializers.CourseSerializer(courses, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = serializers.CourseSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
