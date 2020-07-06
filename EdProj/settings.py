6860395e1186819817ca4c7e16bb180692d65b56
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-06 12:33:40 -0500

    Set default permission classes for rest framework

--- EdProj/settings.py
+++ EdProj/settings.py
@@ -127,4 +127,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
 }
