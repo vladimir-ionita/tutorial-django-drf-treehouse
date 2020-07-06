0e8c162363255ea196211a69419f5719c396be18
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-06 18:01:59 -0500

    Create generic views for `Review` model

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ListCreateCourse.as_view(), name='course_list'),
-    path('<int:pk>/', views.RetrieveUpdateDestroyCourse.as_view(), name='course_detail')
+    path('<int:course_pk>/', views.RetrieveUpdateDestroyCourse.as_view(), name='course_detail'),
+    path('<int:course_pk>/reviews/', views.ListCreateReview.as_view(), name='review_list'),
+    path('<int:course_pk>/reviews/<int:review_pk>', views.RetrieveUpdateDestroyReview.as_view(), name='review_detail')
 ]
--- courses/views.py
+++ courses/views.py
@@ -12,3 +12,16 @@ class ListCreateCourse(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+
+
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
