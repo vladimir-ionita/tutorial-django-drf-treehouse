235bc626f134d1a478e6521306eec9f778420d49
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-06 13:22:32 -0500

    Create serializer for `Review` model

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,6 +3,19 @@ from rest_framework import serializers
 from . import models
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'course',
+            'name',
+            'review',
+            'rating',
+            'created_at'
+        )
+        model = models.Review
+
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
