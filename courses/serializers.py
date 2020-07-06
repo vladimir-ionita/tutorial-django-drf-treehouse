106d551766018aa3cc16454c4769768eac5b3f5d
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-06 13:23:22 -0500

    Make read only the `email` field in `Review` model

--- courses/serializers.py
+++ courses/serializers.py
@@ -5,6 +5,9 @@ from . import models
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
         fields = (
             'id',
             'course',
