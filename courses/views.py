ad69b5dcba3fc786fe4986f818dab13eda8353ab
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-06 18:42:37 -0500

    Use decorators to add a detail view using viewsets

--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
@@ -46,6 +48,14 @@ class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+
+    @action(methods=['get'], detail=True)
+    def reviews(self, request, pk=None):
+        course = self.get_object()
+        serializer = serializers.ReviewSerializer(
+            course.reviews.all(), many=True
+        )
+        return Response(serializer.data)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
